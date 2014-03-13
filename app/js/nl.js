ab27b1ab5230f853aa48fae98a84e0a26c49b946
fram <fm.de.jouvencel@gmail.com>
2014-03-13 15:04:38 +0100

    // make it work even outside www-root + ui

--- app/js/nl.js
+++ app/js/nl.js
@@ -24,23 +24,23 @@ function apiURL(path)
 nl.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 	.when('/', {
-		templateUrl: '/views/index.html',
+		templateUrl: 'views/index.html',
 		controller: 'IndexController'
 	})
 	.when('/newsletters', {
-		templateUrl: '/views/newsletters.html',
+		templateUrl: 'views/newsletters.html',
 		controller: 'NewslettersController'
 	})
 	.when('/newsletters/:newsletter_id', {
-		templateUrl: '/views/newsletter.html',
+		templateUrl: 'views/newsletter.html',
 		controller: 'NewsletterController'
 	})
 	.when('/newsletters/:newsletter_id/:language_code', {
-		templateUrl: '/views/newsletter.html',
+		templateUrl: 'views/newsletter.html',
 		controller: 'NewsletterController'
 	})
 	.when('/messages', {
-		templateUrl: '/views/messages.html',
+		templateUrl: 'views/messages.html',
 		controller: 'MessagesController'
 	});
 }]);
